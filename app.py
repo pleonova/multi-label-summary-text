633f6ea15954c8338195aa0db6cfab3b2ef50eab
Paula Leonova <paulaleonova@paulas-mbp.myfiosgateway.com>
2021-12-09 11:58:24 -0500

    Nest loading spinners

--- app.py
+++ app.py
@@ -37,60 +37,65 @@ with st.spinner('Loading pretrained models...'):
     classifier = load_model()
 st.success('Ready for inference...')
 
+
+
 if submit_button:
     if len(labels) == 0:
         st.write('Enter some text and at least one possible topic to see predictions.')
-
-    # For each body of text, create text chunks of a certain token size required for the transformer
-    nested_sentences = create_nest_sentences(document = text_input, token_max_length = 1024)
-
-    summary = []
-    st.markdown("### Text Chunk & Summaries")
-    st.markdown("Breaks up the original text into sections with complete sentences totaling \
-        less than 1024 tokens, a requirement for the summarizer.")
-
-    # For each chunk of sentences (within the token max), generate a summary
-    for n in range(0, len(nested_sentences)):
-        text_chunk = " ".join(map(str, nested_sentences[n]))
-        st.markdown(f"###### Chunk {n+1}/{len(nested_sentences)}" )
-        st.markdown(text_chunk)
-
-        chunk_summary = summarizer_gen(summarizer, sequence=text_chunk, maximum_tokens = 300, minimum_tokens = 20)
-        summary.append(chunk_summary) 
-        st.markdown("###### Partial Summary")
-        st.markdown(chunk_summary)
-        # Combine all the summaries into a list and compress into one document, again
-        final_summary = " \n".join(list(summary))
-
-    # final_summary = summarizer_gen(summarizer, sequence=text_input, maximum_tokens = 30, minimum_tokens = 100)
-    st.markdown("### Combined Summary")
-    st.markdown(final_summary)
     
-    with st.spinner('Matching labels...'):
-        topics, scores = classifier_zero(classifier, sequence=final_summary, labels=labels, multi_class=True)
-        # st.markdown("### Top Label Predictions: Combined Summary")
-        # plot_result(topics[::-1][:], scores[::-1][:])
-        # st.markdown("### Download Data")
-        data = pd.DataFrame({'label': topics, 'scores_from_summary': scores})
-        # st.dataframe(data)
-        # coded_data = base64.b64encode(data.to_csv(index = False). encode ()).decode()
-        # st.markdown(
-        #     f'<a href="data:file/csv;base64, {coded_data}" download = "data.csv">Download Data</a>',
-        #     unsafe_allow_html = True
-        #     )
+    with st.spinner('Generating summaries...'):
+        # For each body of text, create text chunks of a certain token size required for the transformer
+        nested_sentences = create_nest_sentences(document = text_input, token_max_length = 1024)
 
-        st.markdown("### Top Label Predictions: Summary & Full Text")
-        topics_ex_text, scores_ex_text = classifier_zero(classifier, sequence=example_text, labels=labels, multi_class=True)
-        plot_dual_bar_chart(topics, scores, topics_ex_text, scores_ex_text)
+        summary = []
+        st.markdown("### Text Chunk & Summaries")
+        st.markdown("Breaks up the original text into sections with complete sentences totaling \
+            less than 1024 tokens, a requirement for the summarizer.")
 
-        data_ex_text = pd.DataFrame({'label': topics_ex_text, 'scores_from_full_text': scores_ex_text})
-        data2 = pd.merge(data, data_ex_text, on = ['label'])
-        st.markdown("### Data Table")
-    st.success('Almost done, see download link below.')
-        
-    coded_data = base64.b64encode(data2.to_csv(index = False). encode ()).decode()
-    st.markdown(
-        f'<a href="data:file/csv;base64, {coded_data}" download = "data.csv">Click here to download the data</a>',
-        unsafe_allow_html = True
-        )
-    st.dataframe(data2)
+        # For each chunk of sentences (within the token max), generate a summary
+        for n in range(0, len(nested_sentences)):
+            text_chunk = " ".join(map(str, nested_sentences[n]))
+            st.markdown(f"###### Chunk {n+1}/{len(nested_sentences)}" )
+            st.markdown(text_chunk)
+
+            chunk_summary = summarizer_gen(summarizer, sequence=text_chunk, maximum_tokens = 300, minimum_tokens = 20)
+            summary.append(chunk_summary) 
+            st.markdown("###### Partial Summary")
+            st.markdown(chunk_summary)
+            # Combine all the summaries into a list and compress into one document, again
+            final_summary = " \n".join(list(summary))
+
+        # final_summary = summarizer_gen(summarizer, sequence=text_input, maximum_tokens = 30, minimum_tokens = 100)
+        st.markdown("### Combined Summary")
+        st.markdown(final_summary)
+    
+        with st.spinner('Matching labels...'):
+            topics, scores = classifier_zero(classifier, sequence=final_summary, labels=labels, multi_class=True)
+            # st.markdown("### Top Label Predictions: Combined Summary")
+            # plot_result(topics[::-1][:], scores[::-1][:])
+            # st.markdown("### Download Data")
+            data = pd.DataFrame({'label': topics, 'scores_from_summary': scores})
+            # st.dataframe(data)
+            # coded_data = base64.b64encode(data.to_csv(index = False). encode ()).decode()
+            # st.markdown(
+            #     f'<a href="data:file/csv;base64, {coded_data}" download = "data.csv">Download Data</a>',
+            #     unsafe_allow_html = True
+            #     )
+
+            st.markdown("### Top Label Predictions: Summary & Full Text")
+            topics_ex_text, scores_ex_text = classifier_zero(classifier, sequence=example_text, labels=labels, multi_class=True)
+            plot_dual_bar_chart(topics, scores, topics_ex_text, scores_ex_text)
+
+            data_ex_text = pd.DataFrame({'label': topics_ex_text, 'scores_from_full_text': scores_ex_text})
+            data2 = pd.merge(data, data_ex_text, on = ['label'])
+            st.markdown("### Data Table")
+
+            with st.spinner('Generating a table of results and a download link...'):
+                coded_data = base64.b64encode(data2.to_csv(index = False). encode ()).decode()
+                st.markdown(
+                    f'<a href="data:file/csv;base64, {coded_data}" download = "data.csv">Click here to download the data</a>',
+                    unsafe_allow_html = True
+                    )
+                st.dataframe(data2)
+            st.success('All done!')
+            st.balloons()
